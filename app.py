1e98abe38e61b40d0b280d062758c08ae8d487a4
Oscar <oo89@njit.edu>
2021-04-07 19:14:37 +0000

    Adding models and db code to start

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, json
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
 app = Flask(__name__, static_folder='./build/static')
 
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+
+import models
+db.create_all()
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+socketio = SocketIO(app,
+                    cors_allowed_origins="*",
+                    json=json,
+                    manage_session=False)
 
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
