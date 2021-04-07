b1801842a7a7873026c07f147174bb077e9528e2
Oscar <oo89@njit.edu>
2021-04-07 20:20:57 +0000

    Adding DB info and Google Auth

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import os
-from flask import Flask, send_from_directory, json
+from flask import Flask, send_from_directory, json, redirect, request, url_for
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
 
 load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
@@ -20,6 +29,13 @@ db = SQLAlchemy(app)
 
 import models
 db.create_all()
+
+#Google Auth 
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
