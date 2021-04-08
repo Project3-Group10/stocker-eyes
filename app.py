7ae0f383ad89b5c1ecadfe3c180bb34e63cf093d
Oscar <oo89@njit.edu>
2021-04-08 01:39:44 +0000

    I added the routes that are needed for google, all the secrets. I don't know how to meke them work with react

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, send_from_directory, json, redirect, request, url_for
+from flask import Flask, send_from_directory, json, redirect, request, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
+from datetime import timedelta
 from flask_login import (
     LoginManager,
     current_user,
@@ -18,6 +20,11 @@ load_dotenv(find_dotenv())  # This is to load your env variables from .env
 
 app = Flask(__name__, static_folder='./build/static')
 
+# Session config
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+
 # Point SQLAlchemy to your Heroku database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
@@ -31,10 +38,25 @@ import models
 db.create_all()
 
 #Google Auth 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
+
+#GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+#GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+#GOOGLE_DISCOVERY_URL = (
+#    "https://accounts.google.com/.well-known/openid-configuration"
+#)
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
 )
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -43,12 +65,48 @@ socketio = SocketIO(app,
                     cors_allowed_origins="*",
                     json=json,
                     manage_session=False)
+#Added to auth 
+@app.route('/')
+@login_required
+def hello_world():
+    email = dict(session)['profile']['email']
+    return f'Hello, you are logge in as {email}!'
 
+@app.route('/login')
+def login(): 
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scope
+    user_info = resp.json() 
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
+    
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+#login_manager = LoginManager()
+#login_manager.init_app(app)
 
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from app import db
 
 
-class Person(db.Model):
-    username = db.Column(db.String(80), primary_key=True)
-    score = db.Column(db.Integer, nullable=False)
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    avatar = db.Column(db.String(200))
+    active = db.Column(db.Boolean, default=False)
+    tokens = db.Column(db.Text)
+    #created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return '<User %r>' % self.id
