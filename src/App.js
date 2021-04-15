5910aaf93d7db7f3dde362e49769f799d033c7f0
jvn3 <61201797+jvn3@users.noreply.github.com>
2021-04-14 20:25:05 -0400

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Stock from './Stock.js';
 import Home from './Home';
 import io from 'socket.io-client';
 import {useEffect, useState, useRef} from 'react';
@@ -10,14 +9,10 @@ const socket = io();
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        <GAuth />
-        <Stock />
-=======
       <Home sock={socket} />
->>>>>>> 3312a0ce5ed4d08cbd01beac34601c6cb32d6baa
     </div>
   );
 }
 
 export default App;
+
