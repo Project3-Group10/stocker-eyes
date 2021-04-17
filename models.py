04942f18f606d8d79f094fabaa016e2f18d7aaae
Oscar Ojeda <oo89@njit.edu>
2021-04-17 21:34:57 +0000

    relationship many to many user and stock created, 4 tables working

--- models.py
+++ models.py
@@ -1,17 +1,15 @@
 from app import DB
 
-#Many to many relationship table 
-#association_table = DB.Table('association_table',
- #   DB.Column('id', DB.Integer, primary_key=True), 
-   # DB.Column('user_id', DB.Integer, DB.ForeignKey('user.id')),
-   # DB.Column('stock_id', DB.Integer, DB.ForeignKey('stock.id'))
-  #  )
-
+#Many to many relationship table User and Stocks
+user_identifie = DB.Table('user_identifie',
+    DB.Column('stock_id', DB.Integer, DB.ForeignKey('stocks.stock_id')),
+    DB.Column('user_id', DB.Integer, DB.ForeignKey('users.user_id'))
+)
 #User table 
 class User(DB.Model):
-    #__tablename__ = 'user'
-    #id = DB.Column(DB.Integer, primary_key=True)
-    email = DB.Column(DB.String(100), primary_key=True)
+    __tablename__ = 'users'
+    user_id = DB.Column(DB.Integer, primary_key=True)
+    email = DB.Column(DB.String(100), nullable=True)
     name = DB.Column(DB.String(100), nullable=True)
     avatar = DB.Column(DB.String(200))
     status = DB.Column(DB.Boolean, default=False)
@@ -23,8 +21,8 @@ class User(DB.Model):
 
 #Stock Table 
 class Stock(DB.Model):
-    #__tablename__ = "stock"
-    id = DB.Column(DB.Integer, primary_key=True)
+    __tablename__ = "stocks"
+    stock_id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(20), nullable=False)
     dateDB = DB.Column(DB.String(20), nullable=False)
     open_price = DB.Column(DB.String(100))
@@ -33,8 +31,9 @@ class Stock(DB.Model):
     close_price = DB.Column(DB.String(100))
     adjusted_clase_price = DB.Column(DB.String(100))
     volume_price = DB.Column(DB.String(100))
-    
-   # users = DB.relationship('User', secondary=association_table, backref='Stock')
+    users = DB.relationship("User",
+                               secondary=user_identifie, 
+                               backref=DB.backref('stocks', lazy='dynamic'))
     def __repr__(self):
         return f"Stock('{self.name}', '{self.dateDB}', '{self.close_price}')"
 
