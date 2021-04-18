d1168cddb3ed95aa274f870ab86932d39ee3bc26
yarmout <58712447+yarmout@users.noreply.github.com>
2021-04-18 18:57:42 -0400

    Add router paths

--- src/App.js
+++ src/App.js
@@ -12,12 +12,31 @@ function App() {
   return (
       <Router>
           <div>
+              <nav>
+                  <ul>
+                      <li>
+                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                          <Link to="/register">Register</Link>
+                      </li>
+                      <li>
+                         <Link to="/profile">Profile</Link>
+                      </li>
+                  </ul>
+              </nav>
               {/* To add links to other directories, follow this guide https://reactrouter.com/web/guides/quick-start*/}
               {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
               <Switch>
-                  <Route path="/">
+                  <Route exact path="/">
                       <Home />
+                  </Route>
+                  <Route path="/register">
+                      Register
+                  </Route>
+                  <Route path="/profile">
+                      Profile
                   </Route>
               </Switch>
           </div>
