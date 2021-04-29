5dc347aead32ab17329d936d95735b80e6d98b24
Jose Lazarte <ec2-user@ip-172-31-87-203.ec2.internal>
2021-04-29 03:02:25 +0000

    News rendering issue with map for the homepage

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,43 +3,46 @@ import socket from "./utils/socket";
 import { useState, useEffect } from 'react';
 
 const News = (props) => {
-    
-    socket.on('homeResponse', (groupData) => {
-      console.log(groupData);
-      localStorage.setItem('homeStocks', JSON.stringify(groupData['homeStock']));
-      localStorage.setItem('homeNews', JSON.stringify(groupData['homeNews']));
-    });
-    
+
+    const [homeNews, sethomeNews] = useState(null);
+    console.log('tophomeNews', homeNews, typeof homeNews);
+
+    useEffect(() => {
+        socket.on('homeResponse', (groupData) => {
+            console.log('homeResponse', typeof JSON.parse(JSON.stringify(groupData['homeNews']))['wmtData']);
+            localStorage.setItem('homeStocks', JSON.stringify(groupData['homeStock']));
+            localStorage.setItem('homeNews', JSON.stringify(groupData['homeNews']));
+            
+            const replace = JSON.parse(JSON.stringify(groupData['homeNews']))[props.ticker+'Data']['articles'];
+            console.log(props.ticker+'Data', replace)
+            sethomeNews(replace);
+        });
+    },[]);
+
+
     if (props.rq == "Home") {
-        
-        const newsStates = JSON.parse(localStorage.getItem('homeNews'))[props.ticker+'Data']['articles'];
-        console.log('Render', newsStates);
-        
+        if (props.ticker == "wmt") {
+
+            console.log('Before Render',homeNews == null?'Empty':homeNews);
+
             return (
 
-                <div className={`newsArea ${props.ticker}`}>{
-                    newsStates.map((ix)=>{<p>Hi{ix['title']}</p>})
-                    }
+                <div className={`newsArea ${props.ticker}`}>
                     <div className="gridContainer">
                     
-                    
+                    {console.log('In Render',homeNews)}
                     {
-                    newsStates.map((url)=>{
-                            <div className="iframely-embed">
-                                <a href={url['url']}>
-                                    <div className="iframely-responsive" >
-                                        <img src={url['urlToImage']}/>
-            
-                                         <p className="headline">{url['title']}</p>
-            
-                                    </div>
-                                </a>
+                    
+                    homeNews == null?'Empty':homeNews.map(()=>{
+                            <div>
+                                1
                             </div>
-                        })
+                        ;})
                     }
                     </div>
                 </div>
             );
+        }
     }
 
 };
