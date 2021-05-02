b65a7e36ae4a0159d0f23fdf0881e84db6d8a518
Jose Lazarte <ec2-user@ip-172-31-87-203.ec2.internal>
2021-05-02 02:28:11 +0000

    Fixed issue where charts disappear when user clicks on it

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -24,16 +24,19 @@ const Home = (props) => {
     function expandStock(ele) {
         const box = ele.target.classList[1];
         let expandCopy = { dow: 0, sp: 0, nasdaq: 0 };
-        console.log('ELE',ele);
-        if (expandMain['dow'] == 1 && expandMain['sp'] == 1) {
-            console.log(box);
-            expandCopy[box] = 1;
-            ele.target.style.left = 0;
-            setexpandMain(expandCopy);
-        }
-        else {
-            setexpandMain({ dow: 1, sp: 1, nasdaq: 1 });
-            ele.target.style.left = null;
+
+        if (ele.target.className == 'stockArea dow ' || ele.target.className == 'stockArea sp ' || ele.target.className == 'stockArea nasdaq ') {
+
+            if (expandMain['dow'] == 1 && expandMain['sp'] == 1) {
+                console.log(box);
+                expandCopy[box] = 1;
+                ele.target.style.left = 0;
+                setexpandMain(expandCopy);
+            }
+            else {
+                setexpandMain({ dow: 1, sp: 1, nasdaq: 1 });
+                ele.target.style.left = null;
+            }
         }
 
     }
@@ -42,13 +45,13 @@ const Home = (props) => {
     return (
         <div className="home">
             <div className="mainContainer">
-                <div className={`stockArea dow ${expandMain.dow? '' : 'hide'}`} onClick={expandStock} >
+                <div id="home1" className={`stockArea dow ${expandMain.dow? '' : 'hide'}`} onClick={expandStock} >
                     <h1 className="stockTitle">WMT</h1>
                     <Stock ticker={"wmt"} rq={"Home"}/>
-//                     <News ticker={"wmt"} rq={"Home"}/>
+                    <News ticker={"wmt"} rq={"Home"}/>
 
                 </div>
-                <div className={`stockArea sp ${expandMain.sp? '' : 'hide'}`} onClick={expandStock} >
+                <div id="home2" className={`stockArea sp ${expandMain.sp? '' : 'hide'}`} onClick={expandStock} >
                     <h1 className="stockTitle">OVV</h1>
                     <Stock ticker={"ovv"} rq={"Home"}/>
                     <News ticker={"ovv"} rq={"Home"}/>
@@ -56,7 +59,7 @@ const Home = (props) => {
 
 
                 </div>
-                <div className={`stockArea nasdaq ${expandMain.nasdaq? '' : 'hide'}`} onClick={expandStock} >
+                <div id="home3" className={`stockArea nasdaq ${expandMain.nasdaq? '' : 'hide'}`} onClick={expandStock} >
                     <h1 className="stockTitle">APPL</h1>
                     <Stock ticker={"appl"} rq={"Home"}/>
                     <News ticker={"appl"} rq={"Home"}/>
